921bcbdec19c26e28df282a1e93c6425cb7627ae
Pascal Mahe <pascalmahe@free.fr>
2016-02-19 17:27:40 +0100

    Cleans up
    Big clean up: moves notes to the Notes/ directory, deletes old useless
    files.

---
+++ Notes/failed_requireds.rb
@@ -0,0 +1,8 @@
+require '\Dev\workspace\RPP\bean\common.rb'
+require '/Dev/workspace/RPP/bean/common.rb'
+require 'D:\Dev\workspace\RPP\bean\common.rb'
+require 'D:/Dev/workspace/RPP/bean/common.rb'
+require '.\..\..\bean\common.rb'
+require './../../bean/common.rb'
+require '../../bean/common.rb'
+require '..\..\bean/common.rb'
--- failed_requireds.rb
+++
@@ -1,8 +0,0 @@
-require '\Dev\workspace\RPP\bean\common.rb'
-require '/Dev/workspace/RPP/bean/common.rb'
-require 'D:\Dev\workspace\RPP\bean\common.rb'
-require 'D:/Dev/workspace/RPP/bean/common.rb'
-require '.\..\..\bean\common.rb'
-require './../../bean/common.rb'
-require '../../bean/common.rb'
-require '..\..\bean/common.rb'
